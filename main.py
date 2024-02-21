64a04a049976f121252cb6841eedb0323f755eb2
Deema Rain <deemarain@Deemas-MacBook-Pro.local>
2024-02-21 18:28:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# THE BIO FUNCTION
+
+def bio(name,born,pronoun):
+    age1 = born + 67
+    age2 = 2024 - born
+    print(name, "was born in", born)
+    print(pronoun, "will turn", age2, "this year",)
+    print(pronoun, "will be 67", "in the year",  age1)
+
+
+
+
+
+
+# PRINT BIOS
+bio("stephen",1984,"he")
+bio("mary",1990,"she")
+bio("jane",2000,"she")
+
+
+# EXIT
+quit()
